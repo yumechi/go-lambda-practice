8b09da217d678d70a543752dc5db50b8980b4f3b
yumechi <yumechi0525@gmail.com>
2021-02-20 16:18:13 +0900

    change json keys

--- cmd/lambda_functions.go
+++ cmd/lambda_functions.go
@@ -2,20 +2,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type MyEvent struct {
-	Name string `json:"What is your name?"`
-	Age  int    `json:"How old are you?"`
+	Name string `json:"Name"`
+	Age  int    `json:"Age"`
 }
 
 type MyResponse struct {
-	Message string `json:"Answer:"`
+	Message string `json:"Answer"`
 }
 
-func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
+func HandleLambdaEvent(ctx context.Context, event MyEvent) (MyResponse, error) {
 	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
